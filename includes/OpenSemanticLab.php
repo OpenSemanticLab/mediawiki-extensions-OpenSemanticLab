b77133eb3174c231a414f2815c16a7cc31aff911
Simontaurus <simon.stier@gmx.de>
2022-02-12 13:45:28 +0000

    add tool entry for subpage creation

--- includes/OpenSemanticLab.php
+++ includes/OpenSemanticLab.php
@@ -175,4 +175,26 @@ class OpenSemanticLab {
 		return true;
 
 	}
+
+        public static function onSkinTemplateNavigation( &$skin, &$links ) {
+       		// Add an additional link
+       		//https://stackoverflow.com/questions/18442495/how-to-get-current-page-url-in-mediawiki
+      		$links['views']['subpage'] = array(
+			'class' => false, // false or 'selected', defines whether the tab should be highlighted
+			'text' => 'Unterseite neu erstellen', // what the tab says
+			'href' => "https://$_SERVER[HTTP_HOST]/wiki/CreatePage?superpage=" . $skin->getTitle()->getDBkey(),
+		);
+
+		//not placed properly with Skin:Timeless -> use javascript
+		//$request = $skin->getRequest();
+		//$reveal = $request->getText( 'reveal' );
+		//$links['actions']['slide'] = array(
+		//	'class' => ( $reveal == 'true') ? 'selected' : false,
+		//	'text' => "Slideshow",
+		//	'href' => $skin->makeArticleUrlDetails($skin->getTitle()->getFullText(), 'reveal=true' )['href']
+		//);
+
+        	return true;
+        }
+
 }
