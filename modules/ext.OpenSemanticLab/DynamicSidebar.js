78b46e158a7ad6e4b2438aa4474bf96740407640
SimonTaurus <simon.stier@gmx.de>
2023-02-09 01:52:12 +0000

    don't try to load sidebar if no user is logged in

--- modules/ext.OpenSemanticLab/DynamicSidebar.js
+++ modules/ext.OpenSemanticLab/DynamicSidebar.js
@@ -6,6 +6,7 @@ REL: modules/ext.OpenSemanticLab/DynamicSidebar.js
 //dynamic sitebar content from user subpage
 $(document).ready( function() {
 	const user_name = mw.config.get('wgUserName');
+	if (!user_name) return; //no user not logged in
 	new mw.Api().get( {
 		action: 'parse',
 		page: 'User:' + user_name + '/Config/UI/Sidebar',
